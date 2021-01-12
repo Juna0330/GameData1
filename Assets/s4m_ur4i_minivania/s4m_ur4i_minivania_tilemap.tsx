e2a77fadba58220341d0d3b3d7bf7ea3b202762d
Masaki Onara <ottoaizu330@gmail.com>
2021-01-12 11:08:49 +0900

    初期設定

---
+++ Assets/s4m_ur4i_minivania/s4m_ur4i_minivania_tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="s4m_ur4i_minivania_tilemap" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="s4m_ur4i_minivania_tilemap.png" width="256" height="256"/>
+</tileset>
